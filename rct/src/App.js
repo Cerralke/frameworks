195f3c4384cda7fc1cfea321f841c9ed83ba51c6
Cerralke <jedrzejczykmagdalena93@gmail.com>
2019-12-30 14:19:53 +0100

    uruchamianie na porcie 8000 i ładowanie danych

--- rct/src/App.js
+++ rct/src/App.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section id='datarows'></section>
     );
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    };
+  }
+  componentDidMount() {
+    fetch('http://localhost:1337/datas')
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
   }
 }
 
